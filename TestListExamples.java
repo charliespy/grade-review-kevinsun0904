1c77a4a2f78cbd501540c5cff84de18fc96588ed
charliespy <42754940+charliespy@users.noreply.github.com>
2023-03-13 22:22:33 -0700

    changed grading part

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.beans.Transient;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,10 +14,20 @@ class IsMoon implements StringChecker {
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> left = Arrays.asList("b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test 
+  public void testpass() {
+
+  }
+
+  @Test
+  public void testPass() {
+
   }
 }
